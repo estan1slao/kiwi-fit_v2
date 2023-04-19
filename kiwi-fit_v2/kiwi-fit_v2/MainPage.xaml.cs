3d946cb81ce183487577a36532c623b4d02bb870
ksushasolnyshko <ksupop65@mail.ru>
2023-04-19 16:42:26 +0500

    firstpages

--- kiwi-fit_v2/kiwi-fit_v2/MainPage.xaml.cs
+++ kiwi-fit_v2/kiwi-fit_v2/MainPage.xaml.cs
@@ -6,13 +6,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+[assembly: ExportFont("Ubuntu-Bold.ttf", Alias = "Ubuntu")]
 namespace kiwi_fit_v2
-{
+{  
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            loadingPicture.Source = ImageSource.FromResource("kiwi-fit_v2.startpicture.png");
+            loadingPicture.Aspect = Aspect.Fill;
+        }
+
+        private async void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Registration());
         }
     }
 }
