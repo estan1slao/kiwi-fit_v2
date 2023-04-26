e56d68f186e3e3d1e706adf7377cf898eeaf3cb2
ksushasolnyshko <ksupop65@mail.ru>
2023-04-26 19:00:04 +0500

    начало обучения по шагам

---
+++ kiwi-fit_v2/kiwi-fit_v2/StepInstr1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kiwi_fit_v2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StepInstr1 : ContentPage
+	{
+		public StepInstr1 ()
+		{
+			InitializeComponent ();
+            stepbg.Source = ImageSource.FromResource("kiwi-fit_v2.stepsbg.png");
+            stepbg.Aspect = Aspect.Fill;
+            im9.Source = ImageSource.FromResource("kiwi-fit_v2.im9.png");
+            im9.Aspect = Aspect.Fill;
+
+            im10.Source = ImageSource.FromResource("kiwi-fit_v2.im10.png");
+            im10.Aspect = Aspect.Fill;
+        }
+	}
+}
