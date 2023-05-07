191814975e9a9c3f7f2f8a689716094907f8dc0c
ksushasolnyshko <ksupop65@mail.ru>
2023-05-07 13:27:03 +0500

    добавлены все страницы с обучением

--- kiwi-fit_v2/kiwi-fit_v2/StepInstr2.xaml.cs
+++ kiwi-fit_v2/kiwi-fit_v2/StepInstr2.xaml.cs
@@ -24,5 +24,10 @@ namespace kiwi_fit_v2
             im10.Source = ImageSource.FromResource("kiwi-fit_v2.im10.png");
             im10.Aspect = Aspect.Fill;
         }
-	}
+
+        private async void GoNextPage(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new StepInstr3());
+        }
+    }
 }
---
+++ kiwi-fit_v2/kiwi-fit_v2/StepInstr3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kiwi_fit_v2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StepInstr3 : ContentPage
+	{
+		public StepInstr3 ()
+		{
+			InitializeComponent ();
+            stepbg.Source = ImageSource.FromResource("kiwi-fit_v2.stepsbg.png");
+            stepbg.Aspect = Aspect.Fill;
+
+            menuPic.Source = ImageSource.FromResource("kiwi-fit_v2.menu.png");
+            menuPic.Aspect = Aspect.Fill;
+        }
+	}
+}
