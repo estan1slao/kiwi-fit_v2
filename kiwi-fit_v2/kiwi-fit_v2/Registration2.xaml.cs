cc75b82a4696089abf9b0470ea7ac0cb76dcebfd
ksushasolnyshko <ksupop65@mail.ru>
2023-05-09 14:52:52 +0500

    добавлена вторая страница регистрации

---
+++ kiwi-fit_v2/kiwi-fit_v2/Registration2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kiwi_fit_v2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registration2 : ContentPage
+	{
+		public Registration2 ()
+		{
+			InitializeComponent();
+		}
+
+        private void GainWeightButtonClicked(object sender, System.EventArgs e)
+        {
+        }
+
+        private void LoseWeightButtonClicked(object sender, System.EventArgs e)
+        {
+        }
+
+        private void EndRegistration(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
